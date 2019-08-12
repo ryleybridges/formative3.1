ba8d40b85034fc7adeb5bd09a649ca27f06ff62b
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-08-13 11:47:01 +1200

    Finished setting up npm and server, as well as the public folder

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+      jshint: {
+          files: ["*.js", "js/script.js"],
+          options: {
+              globals:{
+                  jQuery: true
+              }
+          }
+      },
+      jshint: {
+        files: ["*.js", "js/script.js"],
+        options: {
+            globals:{
+                jQuery: true
+            }
+        }
+      }
+    });
+
+
+// load npm tasks
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+// register tasks
+  grunt.registerTask('all', ['sass', 'cssmin']);
+  grunt.registerTask('check', ['jshint']);
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`application is running on port ${port}`));
