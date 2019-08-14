eb5f3b74da7ea5bb37eb7bbfa91c817b17837b2d
ryleybridges <rye.brid@gmail.com>
2019-08-14 15:13:59 +1200

    Added API, got it displaying in console and began with the UI content

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-  $.ajax({
-  url: 'https://newsapi.org/v2/top-headlines?country=nz&apiKey=4eef0c7040a24bd38e258c815585c046',
-  type: 'GET',
-  dataType: 'json',
-  success: function(data){
-    const articles = data.articles;
-    for (var i = 0; i < articles.length; i++) {
-      console.log(articles[i]);
-    }
+    $.ajax({
+      url: 'https://newsapi.org/v2/top-headlines?country=nz&apiKey=4eef0c7040a24bd38e258c815585c046',
+      type: 'GET',
+      dataType: 'json',
+      success: function(data){
+        const articles = data.articles;
+        for (var i = 0; i < articles.length; i++) {
+          console.log(articles[i]);
+        }
+      },
+      error: function(){
+        console.log("not good");
+      }
+    });
 
-  },
-  error: function(){
-    console.log("not good");
-  }
-});
+
 });
