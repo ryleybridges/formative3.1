c83b623b7fc14abfbb4614587bf6fe395f358f23
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-08-15 10:53:12 +1200

    display news in card form

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const path = require('path');
 
+app.use(function(req, res, next){
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
 
-
-
-
-
-
-
-
-
-
-
+app.use(express.static('./public'));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/popper', express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd')));
 
 app.listen(port, () => console.log(`application is running on port ${port}`));
